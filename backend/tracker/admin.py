c0d8fc0bd1b775581d90f0a3deccbd6dad19dbae
WeWe-power <dmant.rodionov@gmail.com>
2022-04-16 17:20:34 +0300

    Made admin panel more convenient

--- backend/tracker/admin.py
+++ backend/tracker/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import User, Item, ItemPriceRecord
 
-admin.site.register(User)
-admin.site.register(Item)
-admin.site.register(ItemPriceRecord)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email")
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ("name", "brand", "vendor_code", 'provider')
+
+
+@admin.register(ItemPriceRecord)
+class ItemPriceRecordAdmin(admin.ModelAdmin):
+    list_display = ("item", "price", "price_with_sale", 'time_parsed')
 
