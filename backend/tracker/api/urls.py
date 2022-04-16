275daaafba58717f79d2815017105cc87bc4af8b
WeWe-power <dmant.rodionov@gmail.com>
2022-04-16 20:45:00 +0300

    FIX: Added api endpoint in urls file

--- backend/tracker/api/urls.py
+++ backend/tracker/api/urls.py
@@ -5,6 +5,7 @@ from tracker.api import viewsets
 urlpatterns = [
     path('items', viewsets.ItemListCreateView.as_view(), name='item-list'),
     path('items/<str:pk>', viewsets.ItemRetrieveDestroyUpdateView.as_view(), name='item-detail'),
+    path('items/price_history/<str:pk>', viewsets.GetItemPriceHistory.as_view(), name='item-price-history'),
     path('user/items', viewsets.UserItemList.as_view(), name='user-items'),
     path('user/items/<str:pk>', viewsets.UserItemAddDelete.as_view(), name='user-items-delete'),
     path('', include('api.urls')),
